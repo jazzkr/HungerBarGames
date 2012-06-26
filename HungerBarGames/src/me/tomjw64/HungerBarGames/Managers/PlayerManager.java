ef87acf7dcb78c4017804e009bc781e0c9bec84d
Thomas <Thomas@Cooler_Master>
2012-06-25 23:29:26 -0500

    Added PlayerStorage class, which will store the player's state before the game.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Managers/PlayerManager.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Managers/PlayerManager.java
@@ -3,15 +3,15 @@ package me.tomjw64.HungerBarGames.Managers;
 import java.util.HashMap;
 import java.util.Map;
 
-import me.tomjw64.HungerBarGames.Util.PlayerRestore;
+import me.tomjw64.HungerBarGames.Util.PlayerStorage;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class PlayerManager {
-	private static Map<Player,PlayerRestore> inGame=new HashMap<Player,PlayerRestore>();
-	private static Map<Player,PlayerRestore> specing=new HashMap<Player,PlayerRestore>();
-	private static Map<Player,PlayerRestore> respawns=new HashMap<Player,PlayerRestore>();
+	private static Map<Player,PlayerStorage> inGame=new HashMap<Player,PlayerStorage>();
+	private static Map<Player,PlayerStorage> specing=new HashMap<Player,PlayerStorage>();
+	private static Map<Player,PlayerStorage> respawns=new HashMap<Player,PlayerStorage>();
 	
 	public static boolean isInGame(Player p)
 	{
