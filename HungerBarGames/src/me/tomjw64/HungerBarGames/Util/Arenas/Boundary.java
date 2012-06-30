768034bb1685667799a063a0cc191eb397f6e4f7
Thomas <Thomas@Cooler_Master>
2012-06-29 21:43:30 -0500

    Fixed boundaries applying to all worlds, not just the one they are created in.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Util/Arenas/Boundary.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Util/Arenas/Boundary.java
@@ -24,19 +24,26 @@ public class Boundary {
 	
 	public boolean isIn(Block b)
 	{
-		int x=center.getX();
-		int z=center.getZ();
-		int bx=b.getX();
-		int bz=b.getZ();
-		if(type==BoundaryType.CIRCLE)
+		if(world.equals(b.getWorld()))
 		{
-			int asquared=(int)Math.pow(Math.abs(x-bx),2);
-			int bsquared=(int)Math.pow(Math.abs(z-bz),2);
-			return Math.sqrt(asquared+bsquared)<=radius;
+			int x=center.getX();
+			int z=center.getZ();
+			int bx=b.getX();
+			int bz=b.getZ();
+			if(type==BoundaryType.CIRCLE)
+			{
+				int asquared=(int)Math.pow(Math.abs(x-bx),2);
+				int bsquared=(int)Math.pow(Math.abs(z-bz),2);
+				return Math.sqrt(asquared+bsquared)<=radius;
+			}
+			else
+			{
+				return Math.abs(x-bx)<=radius&&Math.abs(z-bz)<=radius;
+			}
 		}
 		else
 		{
-			return Math.abs(x-bx)<=radius&&Math.abs(z-bz)<=radius;
+			return false;
 		}
 	}
 	
